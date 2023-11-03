153d47e624903a2a10910c7b07239f7ad1d8c01e
dastaeren <03230051.gcbs@rub.edu.bt>
2023-11-03 05:42:47 -0700

    added code

--- Eat the gum gum.py
+++ Eat the gum gum.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import time
+from pygame import mixer
 
 # Initialize Pygame
 pygame.init()
@@ -20,13 +21,29 @@ room_surface = pygame.image.load('room.jpg')
 pygame.display.set_caption("Eat the GumGum!")
 font = pygame.font.Font(None, 50)
 
+# Welcome text
+welcome_text_font = pygame.font.Font(None,30)
+welcome_text = welcome_text_font.render("Welcome!",True, (225,255,225))
+welcome_text_rect = welcome_text.get_rect()
+welcome_text_rect.center = (280,230)
+
+welcome_text_font1 = pygame.font.Font(None,23)
+welcome_text1 = welcome_text_font1.render("Let's see how much luffy can eat gum-gum fruit in one minute",True, (225,225,25))
+welcome_text_rect1 = welcome_text1.get_rect()
+welcome_text_rect1.center = (280,250)
+
+# Display the welcome text
+screen.blit(welcome_text, welcome_text_rect,)
+screen.blit(welcome_text1, welcome_text_rect1,)
+pygame.display.flip()
+pygame.time.delay(3000)
+
 # Load images
 luffy = pygame.image.load('luffy.png')
-
 gumgum = pygame.image.load('gumgum.png')
 
 # Scale Images
-luffy = pygame.transform.scale(luffy, (130, 130))
+luffy = pygame.transform.scale(luffy, (150, 130))
 gumgum = pygame.transform.scale(gumgum, (70, 120))
 
 # Initialize position of gumgum
@@ -43,7 +60,8 @@ score = 0
 # Set the initial time
 initial_time = pygame.time.get_ticks()
 
-
+# Load sound
+mixer.music.load('bites.mp3')
 
 # Game loop
 running = True
@@ -52,9 +70,6 @@ while running:
     elapsed_time = time.time() - start_time
     if elapsed_time >= 60:
         running = False
-
-    # Fill the screen with a black background
-    screen.fill((0, 0, 0))
 
     # Get events from the user
     for event in pygame.event.get():
@@ -65,11 +80,11 @@ while running:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and luffy_x > 0:
         luffy_x -= 5
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and luffy_x < WIDTH - 150:
         luffy_x += 5
     if keys[pygame.K_UP] and luffy_y > 0:
         luffy_y -= 5
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_DOWN] and luffy_y < HEIGHT - 130:
         luffy_y += 5
 
     # Draw the background image
@@ -84,11 +99,15 @@ while running:
     # Check for collision between luffy and gumgum
     if luffy_x + 100 > gumgum_x and luffy_x < gumgum_x + 100 and luffy_y + 100 > gumgum_y and luffy_y < gumgum_y + 100:
         score += 1
+       
         gumgum_x = random.randint(0, WIDTH - 100)
         gumgum_y = random.randint(0, HEIGHT - 100)
 
+        # Play the sound when luffy eats gum gum
+        mixer.music.play()
+
     # Display the score
-    font = pygame.font.Font(None, 36)
+    font = pygame.font.Font(None, 30)
     text = font.render("Score: " + str(score), True, (255, 255, 255))
     screen.blit(text, (10, 10))
 
@@ -100,13 +119,14 @@ while running:
         print("One minute has passed.")
         initial_time = current_time
 
-        # Draw the timer
+    # Draw the timer
     text = font.render(f"Time: {int((current_time - initial_time) / 1000)} seconds", True, (255, 255, 255))
-    screen.blit(text, (10, 50))
+    screen.blit(text, (10, 40))
 
-
-
-    # Update the display
+   # Update the screen
     pygame.display.flip()
 
 pygame.quit()
+
+# Display score
+print('Your score is:', score)
